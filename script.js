88821f42807da96c03e11f704578d995d6462392
SteveMarlon <159232325+SteveMarlon@users.noreply.github.com>
2024-11-22 13:20:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//show uploaded image
-// function imagePreview() {
-//     const fileInput = document.getElementById("image");
-//     const file = fileInput.files[0];
-//     if (file && file.size > 2 * 1024 * 1024) { // 2MB in bytes
-//         alert('Error: Image file size exceeds 2MB.');
-//         fileInput.value = ''; // Clear the file input
-//     } else {
-//         if (file) {
-//             const reader = new FileReader();
-//             reader.onload = function(e) {
-//                 const previewImage = document.getElementById("imagePreview");
-//                 previewImage.src = e.target.result;
-//                 previewImage.style.display = "block"; // Show the image
-//             };
-//             reader.readAsDataURL(file); // Read the file as a data URL
-//         }
-//     } 
-// }
-
 function showImage() {
     const fileInput = document.getElementById("image");
     const file = fileInput.files[0];
